1b020793715c54c6a7ec1006004d6927c67e2a04
Anderson Mesquita <andersonvom@gmail.com>
2017-01-07 23:07:10 -0500

    Make login/error handling methods private

--- lib/adp-downloader/http_client.rb
+++ lib/adp-downloader/http_client.rb
@@ -11,22 +11,7 @@ module ADPDownloader
       headers = {"Accept" => "application/json, text/plain, */*"}
       @agent = Mechanize.new {|a| a.request_headers = headers}
       res = _login(Config.credentials)
-      _exit_on_error(res)
-    end
-
-    def _login(creds)
-      @agent.post(LOGIN_URL, {
-        "target" => TARGET_URL,
-        "user" => creds["username"],
-        "password" => creds["password"],
-      })
-    end
-
-    def _exit_on_error(res)
-      uri = res.uri.to_s.downcase
-      if not uri.start_with? TARGET_URL or uri.include? "login"
-        raise "Unable to authenticate: make sure your username and password are correct"
-      end
+      _raise_on_error(res)
     end
 
     def get(url)
@@ -41,6 +26,22 @@ module ADPDownloader
       contents = @agent.get(url).body
       File.open(save_path, 'w') {|f| f.write(contents)} if save_path
       contents
+    end
+
+    private
+    def _login(creds)
+      @agent.post(LOGIN_URL, {
+        "target" => TARGET_URL,
+        "user" => creds["username"],
+        "password" => creds["password"],
+      })
+    end
+
+    def _raise_on_error(res)
+      uri = res.uri.to_s.downcase
+      if not uri.start_with? TARGET_URL or uri.include? "login"
+        raise "Unable to authenticate: make sure your username and password are correct"
+      end
     end
   end
 end
