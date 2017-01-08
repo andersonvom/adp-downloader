a87d39372a9130527076d587b572ef4b28756cde
Anderson Mesquita <andersonvom@gmail.com>
2017-01-07 19:35:50 -0500

    Save all errors to temp file and report back to user

--- lib/adp-downloader.rb
+++ lib/adp-downloader.rb
@@ -5,7 +5,22 @@ require_relative './adp-downloader/pay_statement'
 module ADPDownloader
   def self.download
     http_client = HttpClient.new
-    downloader = Downloader.new(http_client)
-    downloader.get_all_statements
+    begin
+      downloader = Downloader.new(http_client)
+      downloader.get_all_statements
+    rescue Exception => e
+      path = log_exception_to_file(e)
+      puts "An error ocurred. Details were logged to #{path}"
+      exit 1
+    end
+  end
+
+  private
+  def self.log_exception_to_file(e)
+    file = Tempfile.new
+    file.puts(e)
+    file.puts(e.backtrace)
+    FileUtils.mv file.path, File.join("", "tmp")
+    File.join("", "tmp", File.basename(file.path))
   end
 end
