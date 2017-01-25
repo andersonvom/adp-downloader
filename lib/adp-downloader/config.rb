aff5930764a7fe910832082640be9cbb2d0a7390
Anderson Mesquita <andersonvom@gmail.com>
2017-01-24 19:26:04 -0500

    Get rid of 'self.' in Config

--- lib/adp-downloader/config.rb
+++ lib/adp-downloader/config.rb
@@ -4,28 +4,30 @@ require "adp-downloader/constants"
 
 module ADPDownloader
   class Config
-    def self.credentials
-      from_netrc or from_stdin
-    end
+    class << self
+      def credentials
+        from_netrc or from_stdin
+      end
 
-    private
-    def self.creds(username, password)
-      { username: username, password: password }
-    end
+      private
+      def creds(username, password)
+        { username: username, password: password }
+      end
 
-    def self.from_netrc
-      n = Netrc.read
-      username, password = n[MACHINE_NAME]
-      creds(username, password) if username and password
-    end
+      def from_netrc
+        n = Netrc.read
+        username, password = n[MACHINE_NAME]
+        creds(username, password) if username and password
+      end
 
-    def self.from_stdin
-      print "Username: "
-      username = STDIN.gets.chomp
-      print "Password: "
-      password = STDIN.noecho(&:gets).chomp
-      puts
-      creds(username, password)
+      def from_stdin
+        print "Username: "
+        username = STDIN.gets.chomp
+        print "Password: "
+        password = STDIN.noecho(&:gets).chomp
+        puts
+        creds(username, password)
+      end
     end
   end
 end
